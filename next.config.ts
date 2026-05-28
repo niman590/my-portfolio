e8119844ba13ada07bee793249c06ec12db1ff69
niman590 <nimannethmika@gmail.com>
2026-05-28 15:35:59 +0530

    Page seperation

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-portfolio",
-  assetPrefix: "/my-portfolio/",
+  basePath: isProduction ? "/my-portfolio" : "",
+  assetPrefix: isProduction ? "/my-portfolio/" : "",
   images: {
     unoptimized: true,
   },
